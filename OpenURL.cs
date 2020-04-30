77773d321a323567971d1dd62769ee8c35218c9d
BryceAudio <32772344+BryceAudio@users.noreply.github.com>
2020-04-30 14:08:22 +0100

    Add files via upload

---
+++ OpenURL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenURL : MonoBehaviour
+{
+
+
+    void Start() //this wouldn't be in Start - stick this in your 'end game' script.
+                   // e.g. if {playerDone == true...
+
+
+    {
+        Application.OpenURL("https://s.chkmkt.com/?e=195781&h=EDD1DF22921998D&l=en&q1=" + SystemInfo.deviceUniqueIdentifier);
+    }
+    }
